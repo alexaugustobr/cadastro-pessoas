94a50bfd5f1615303fa958e4203110395342fb60
William <william.balan.machado@gmail.com>
2020-08-19 00:43:49 -0300

    Adicionado segurança utilizando Google OAuth2

---
+++ src/main/java/br/com/softplan/pessoas/core/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package br.com.softplan.pessoas.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**").authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
