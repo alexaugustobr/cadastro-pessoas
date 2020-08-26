1fc7f359ff7602f42faa5efc3634975cb9b7e77b
william-balan <william.machado@algaworks.com>
2020-08-26 16:43:22 -0300

    Adicionado regra de Cors

--- src/main/java/br/com/softplan/pessoas/core/security/SecurityConfig.java
+++ src/main/java/br/com/softplan/pessoas/core/security/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
             .antMatcher("/**").authorizeRequests()
             .antMatchers("/**/source").permitAll()
             .anyRequest().authenticated()
