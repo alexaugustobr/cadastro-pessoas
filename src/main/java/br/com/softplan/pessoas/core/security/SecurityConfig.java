4e5d54f1b6899d46d31a392740eb2ccc647219ee
William <william.balan.machado@gmail.com>
2020-08-27 16:23:55 -0300

    Liberando acesso a documentação Swagger

--- src/main/java/br/com/softplan/pessoas/core/security/SecurityConfig.java
+++ src/main/java/br/com/softplan/pessoas/core/security/SecurityConfig.java
@@ -2,15 +2,16 @@ package br.com.softplan.pessoas.core.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http
             .antMatcher("/**").authorizeRequests()
             .antMatchers("/**/source").permitAll()
             .anyRequest().authenticated()
@@ -19,5 +20,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.NEVER);
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    	web.ignoring().antMatchers(
+    			"/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "/webjars/**");
     }
 }
